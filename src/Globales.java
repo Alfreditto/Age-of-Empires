98b530a37c203f7837f5ef0ea4be166e5eac75ce
Jaime <lnote@outlook.es>
2022-03-10 13:25:46 +0100

    Añadidas mas civilizaciones como POC.

--- src/Globales.java
+++ src/Globales.java
@@ -1,7 +1,7 @@
 public class Globales {
-    public static enum civilizaciones {Espanioles, Bizantino};
+    public static enum civilizaciones {Espanioles, Bizantino, Germanos, Locos, Adolfos};
     public static enum probabilidad {
-        Espaniol(0.4), Bizantino(0.2);
+        Espaniol(0.4), Bizantino(0.2), Germanos(0.6), Locos(0.3), Adolfos(0.2);
 
         private final double value;
 
