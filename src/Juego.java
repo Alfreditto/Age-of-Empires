61c2534f9a1c7cb6a28e9a3127c1b7329a8f9352
Hanndel qwerty <Lnote@outlook.es>
2022-03-13 18:38:20 +0100

    Todo ok, aumentar el tiempo si se quiere

--- src/Juego.java
+++ src/Juego.java
@@ -1,9 +1,12 @@
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Juego {
 
-    Map<Globales.civilizaciones, Civilizacion> civilizaciones = new HashMap<Globales.civilizaciones, Civilizacion>();
+    Map<Globales.civilizaciones, Civilizacion> civilizaciones = new HashMap<>();
 
-    public Juego() {
+    public Juego() throws InterruptedException {
         System.out.println("Bienvenido a una nueva partida de Age of Empires DAM Edition");
         crearCivilizaciones();
         Mina m1 = new Mina();
@@ -11,7 +14,7 @@ public class Juego {
     }
 
 
-    private void crearCivilizaciones(){
+    private void crearCivilizaciones() {
         int counter = 0;
         for (Globales.civilizaciones civilizaciones : Globales.civilizaciones.values()) {
             Civilizacion civ = new Civilizacion(civilizaciones, Globales.probabilidad.values()[counter]);
@@ -20,37 +23,42 @@ public class Juego {
         }
     }
 
-    private void simulacion(Mina mina){
+    private void simulacion(Mina mina) throws InterruptedException {
         int segundos = 0;
-        while(segundos < 60 && mina.getCantera() > 0){
+        while (segundos < 60 && mina.getCantera() > 0) {
             int counter = segundos;
             civilizaciones.forEach(
-                (i, j) ->{
-                    if (counter % 2 == 0){
-                        j.crecerONo();
-                    }
-                    if (counter % 5 == 0 && i == Globales.civilizaciones.Bizantino){
-                        boolean buscando = true;
-                        while(buscando){
-                            Collection<Civilizacion> values = civilizaciones.values();
-                            Civilizacion civ = (Civilizacion) values.toArray()[(int)(Math.random()*(values.size()-1-0)+0)];
-                            if (j != civ){
-                                j.robarAldeano(civ);
-                                buscando = !buscando;
-                            };
+                    (i, j) -> {
+                        if (counter % 2 == 0) {
+                            j.crecerONo();
+
+                        }
+                        if (counter % 5 == 0 && i == Globales.civilizaciones.Bizantino) {
+                            boolean buscando = true;
+                            while (buscando) {
+                                Collection<Civilizacion> values = civilizaciones.values();
+                                Civilizacion civ = (Civilizacion) values.toArray()[(int) (Math.random() * (values.size() - 1) + 0)];
+                                if (j != civ) {
+                                    j.robarAldeano(civ);
+                                    buscando = false;
+                                }
+                            }
+                        }
+                        j.aumentarAlmacen(j.getAldeanos().size());
+                        mina.extraidos(j.getAldeanos().size(), j);
+                        try {
+                            Thread.sleep((1000/civilizaciones.values().size()));
+                        } catch (InterruptedException e) {
                         }
                     }
-                    j.aumentarAlmacen(j.getAldeanos().size());
-                    mina.extraidos(j.getAldeanos().size());
-                }
             );
             segundos++;
         }
         civilizaciones.forEach(
-            (i, j) ->{
-                String civ = String.format("Los %s, gobernados por %s han acabado con %s recursos y %s aldeanos",j.getCivilizacion().toString(), j.GetRey(), j.getAlmacen(), j.getAldeanos().size());
-                System.out.println(civ);
-            }
+                (i, j) -> {
+                    String civ = String.format("Los %s, gobernados por %s han acabado con %s recursos y %s aldeanos", j.getCivilizacion().toString(), j.GetRey(), j.getAlmacen(), j.getAldeanos().size());
+                    System.out.println(civ);
+                }
         );
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ Release 1
 */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 	    new Juego();
     }
 }
